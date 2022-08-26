d0b35b2537bbe1e3391dfb4f0e32103629a64d87
RRay6 <rray6@gmu.edu>
2022-08-26 17:14:15 -0400

    Login

---
+++ client/src/Pages/Login.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import axios from "axios";
+// import { useNavigate } from "react-router-dom";
+// import toast, { Toaster } from "react-hot-toast";
+
+export default function Login() {
+  return <div>Login</div>;
+}
